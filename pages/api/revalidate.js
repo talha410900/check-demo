2c1da5dfab0cc5a40d6beb8a83dc2297fb0d2eec
appledefender <appledefender@gmail.com>
2024-01-10 18:48:37 +0500

    changes

---
+++ pages/api/revalidate.js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+  try {
+    await res.revalidate('/');
+    return res.json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).send('Error revalidating');
+  }
+}
+
